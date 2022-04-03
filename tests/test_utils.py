a8b9a8e70456e311962afa163c177a91bcc10d20
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2022-04-03 11:06:08 +0100

    Add _clip_scores test

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,14 @@
 import pytest
 
-from ..text_explore.utils import _clip_scores
+from text_explore.utils import _clip_scores
 
 def test_clip_cores():
-    pass
+    assert _clip_scores(203, max_=100, min_=0) == 100
+    assert _clip_scores(-4, max_=100, min_=0) == 0
+    assert _clip_scores(50, max_=45, min_=1) == 45
+    assert _clip_scores(4, max_=34, min_=7) == 7
+
+    with pytest.raises(TypeError):
+        assert _clip_scores("Gary", max_=34, min_=7) == 7
+        assert _clip_scores(4, max_="max", min_=7) == 7
+        assert _clip_scores(50, max_=34, min_="min") == 7
