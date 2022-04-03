383b70eb11d84e10fd46d1feb0dcf94e61ad97b9
Temiloluwa Awoyele <awoyeletemiloluwa@gmail.com>
2022-04-03 11:05:27 +0100

    Add Type guard clause

---
+++ tests/test_utils.py
@@ -0,0 +1,6 @@
+import pytest
+
+from ..text_explore.utils import _clip_scores
+
+def test_clip_cores():
+    pass
--- text_explore/utils.py
+++ text_explore/utils.py
@@ -1,20 +1,31 @@
-def _clip_scores(score: float, max_: float, min_: float) -> float:
+from typing import Union
+
+
+def _clip_scores(score: Union[float, int], max_: Union[float, int], min_: Union[float, int]) -> float:
     """
     Clip scores within range.
 
     Arguments:
     ---------
-    score: float
+    score: float | int
         Score to ensure it's within range
-    max_: float
+    max_: float | int
         Maximum value possible
-    min_: float
+    min_: float | int
         Minimum value possible
 
     Returns:
     --------
     clipped_score: float
     """
+    def type_guard(value: Union[float, int], name: str):
+        if not isinstance(value, (float, int)):
+            raise TypeError(f"{name} must be a float or an integer, not a {type(value)}")
+    
+    type_guard(score, "Score")
+    type_guard(max_, "Maximum")
+    type_guard(min_, "Minimum")
+
     if score > max_:
         return float(max_)
     elif score < min_:
